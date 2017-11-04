6f0bca45320cb2fc262528ccc0cd4f8e0cd29c79
Tokutaro Shimada <tokutaro@gmail.com>
2017-11-04 13:06:28 +0900

    stdenv-rhel7-docker-registry/packer.json includes stdenv_rhel docker file

--- stdenv-rhel7-docker-registry/packer_build.bat
+++ stdenv-rhel7-docker-registry/packer_build.bat
@@ -1,8 +1,12 @@
 @echo off
 cd %~dp0
 set PATH=%~dp0..\bin;%PATH%
-set /p RHEL_USER=RHEL subscription username:
-set /p RHEL_PASSWORD=RHEL subscription password:
+if "%RHEL_USER%" == "" (
+  set /p RHEL_USER=RHEL subscription username:
+)
+if "%RHEL_PASSWORD%" == "" (
+  set /p RHEL_PASSWORD=RHEL subscription password:
+)
 echo username=%RHEL_USER%
 echo password=%RHEL_PASSWORD%
 packer.exe validate packer.json
