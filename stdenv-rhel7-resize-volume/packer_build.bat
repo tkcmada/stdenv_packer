e7e856553139bc8b30c522976e1d0319131d7cbd
Tokutaro Shimada <tokutaro@gmail.com>
2017-11-07 22:59:52 +0900

    added localrepo, noip to stdenv-rhel7-repo

--- stdenv-rhel7-resize-volume/packer_build.bat
+++ stdenv-rhel7-resize-volume/packer_build.bat
@@ -1,14 +1,6 @@
 @echo off
 cd %~dp0
 set PATH=%~dp0..\bin;%PATH%
-if "%RHEL_USER%" == "" (
-  set /p RHEL_USER=RHEL subscription username:
-)
-if "%RHEL_PASSWORD%" == "" (
-  set /p RHEL_PASSWORD=RHEL subscription password:
-)
-echo username=%RHEL_USER%
-echo password=%RHEL_PASSWORD%
 packer.exe validate packer.json
-packer.exe build    -on-error=ask packer.json
+packer.exe build    -on-error=cleanup packer.json
 pause
