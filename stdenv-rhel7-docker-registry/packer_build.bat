23669ba5b4e9e62490553867c19d15ba00ffb17e
Tokutaro Shimada <tokutaro@gmail.com>
2017-11-17 01:09:45 +0900

    fixed stdenv-rhel7-docker-registry

--- stdenv-rhel7-docker-registry/packer_build.bat
+++ stdenv-rhel7-docker-registry/packer_build.bat
@@ -1,14 +1,14 @@
 @echo off
 cd %~dp0
 set PATH=%~dp0..\bin;%PATH%
-if "%RHEL_USER%" == "" (
-  set /p RHEL_USER=RHEL subscription username:
-)
-if "%RHEL_PASSWORD%" == "" (
-  set /p RHEL_PASSWORD=RHEL subscription password:
-)
-echo username=%RHEL_USER%
-echo password=%RHEL_PASSWORD%
+rem if "%RHEL_USER%" == "" (
+rem   set /p RHEL_USER=RHEL subscription username:
+rem )
+rem if "%RHEL_PASSWORD%" == "" (
+rem   set /p RHEL_PASSWORD=RHEL subscription password:
+rem )
+rem echo username=%RHEL_USER%
+rem echo password=%RHEL_PASSWORD%
 packer.exe validate packer.json
 packer.exe build    -on-error=cleanup packer.json
 pause
