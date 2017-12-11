7073d52348d1fc4b0676500326c1bde1fa8dc51c
Tokutaro Shimada <tokutaro@gmail.com>
2017-12-11 09:51:13 -0500

    fixed redhatrepo

--- stdenv-redhatrepo/build-shutdown.sh
+++ stdenv-redhatrepo/build-shutdown.sh
@@ -1,3 +1,10 @@
+export RHEL_USER=MUMSS_ASKA
+
+if [ "$RHEL_PASSWORD" = "" ];
+then
+  echo "RHEL_PASSWORD envvar is not set"
+  exit 1 
+fi
 export PATH=../bin:$PATH
 packer validate packer.json \
 && packer build    -on-error=cleanup packer.json \
--- stdenv-redhatrepo/subscription-enable-repo.sh
+++ stdenv-redhatrepo/subscription-enable-repo.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+RHEL_USER=$1
+RHEL_PASSWORD=$2
+shift
+shift
+
 echo "RHEL_USER:$RHEL_USER"
 echo "RHEL_PASSWORD:$RHEL_PASSWORD"
-subscription-manager register --name="docker_$HOSTNAME" --username=$RHEL_USER --password=$RHEL_PASSWORD || exit 1
+
+if [ "$RHEL_USER" = "" ]
+then
+  echo "RHEL_USER is not set yet"
+  exit 1
+fi
+
+if [ "$RHEL_PASSWORD" = "" ]
+then
+  echo "RHEL_PASSWORD is not set yet"
+  exit 1
+fi
+
+echo "registering..."
+subscription-manager register --name="ec2_$HOSTNAME" --username=$RHEL_USER --password=$RHEL_PASSWORD || exit 1
 subscription-manager list --available > subscription-available.txt || exit 1
 python subscription-find-pool.py < subscription-available.txt > pool.txt || exit 1
-subscription-manager subscribe --pool=`cat /root/pool.txt` || exit 1
+echo "subscribing pool..."
+subscription-manager subscribe --pool=`cat pool.txt` || exit 1
 for repo in $@
 do
   echo enabling $repo
